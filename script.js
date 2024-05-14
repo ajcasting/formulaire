347e21cd6d2d5431b87d8ec2d96badb613ab6406
ajcasting <dominator76.tuto@gmail.com>
2024-05-14 21:33:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+if (Math.random() > 0.5) {
+  window.location.href = "https://5e2c5b07-6706-4e8e-94ac-2fef180ac348-00-1hz2bn6n06mej.janeway.replit.dev/";
+} else {
+  window.location.href = "https://5e2c5b07-6706-4e8e-94ac-2fef180ac348-00-1hz2bn6n06mej.janeway.replit.dev/";
+}
